30a51bc34f5f57a7b026e0f0f43f69c40aa35ee3
256hax <256hax@gmail.com>
2024-07-12 23:51:29 +0900

    feat: change network from mainnet to devnet

--- shared/connection.ts
+++ shared/connection.ts
@@ -1,3 +1,4 @@
 import { clusterApiUrl, Connection } from '@solana/web3.js';
 
-export const connection = new Connection(clusterApiUrl('mainnet-beta'));
+// export const connection = new Connection(clusterApiUrl('mainnet-beta'));
+export const connection = new Connection(clusterApiUrl('devnet'));
